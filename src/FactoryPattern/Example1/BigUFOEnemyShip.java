d63033cdcbd499a29cbcab2bce0c6722e5afd763
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-27 15:13:24 +0200

    Created subclass of UFOEnemy

---
+++ src/FactoryPattern/Example1/BigUFOEnemyShip.java
@@ -0,0 +1,9 @@
+package FactoryPattern.Example1;
+
+public class BigUFOEnemyShip extends UFOEnemyShip{
+
+    public BigUFOEnemyShip(){
+        setName("Big UFO Enemy");
+        setAmtDamage(40.0);
+    }
+}
