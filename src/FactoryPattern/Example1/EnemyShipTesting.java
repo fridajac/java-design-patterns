fedb80bfd1aaaf9dd8b707f9d635bbb96d1ea740
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-27 15:05:49 +0200

    Implemented code for example for now not to implement the factory pattern

--- src/FactoryPattern/Example1/EnemyShipTesting.java
+++ src/FactoryPattern/Example1/EnemyShipTesting.java
@@ -1,8 +1,32 @@
 package FactoryPattern.Example1;
 
+import java.util.Scanner;
+
 public class EnemyShipTesting {
 
+    /*
+    Example below shows how we do this WITHOUT the Factory pattern.
+     */
     public static void main(String[] args) {
+        EnemyShip theEnemy = null;
+        Scanner userInput = new Scanner(System.in);
+        String enemyShipOption = "";
+        System.out.println("What type of ship? (U / R)");
+        if(userInput.hasNextLine()){
+            enemyShipOption = userInput.next();
+        }
+        if(enemyShipOption.equals("U")){
+            theEnemy = new UFOEnemyShip();
+        }
+        else{
+            theEnemy = new RocketEnemyShip();
+        }
+        doStuffEnemu(theEnemy);
+    }
 
+    private static void doStuffEnemu(EnemyShip enemyShip) {
+        enemyShip.displayEnemyShip();
+        enemyShip.followHeroShip();
+        enemyShip.enemyShipShoots();
     }
 }
